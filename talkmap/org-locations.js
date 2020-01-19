22513c959e3fc33aa172d856672c9dbed6c5e793
bdoptoelectronics <50175484+bdoptoelectronics@users.noreply.github.com>
2020-01-19 22:39:13 +0000

    update location GPS

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,37 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Munich, Germany",
+    48.149416,
+    11.581191
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Cambridge, UK",
+    52.210166,
+    0.091325
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Tokyo, Japan",
+    35.664885,
+    139.699403
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Japan",
+    33.631760,
+    130.518266
+  ],
+  [
+    "Hasselt, Belgium",
+    50.933877,
+    5.342422
+  ],
+  [
+    "Amsterdam, Netherlands",
+    52.356652,
+    4.949327
+  ],
+  [
+    "Lisbon, Portugal",
+    38.731555,
+    -9.139606
   ]
 ];
